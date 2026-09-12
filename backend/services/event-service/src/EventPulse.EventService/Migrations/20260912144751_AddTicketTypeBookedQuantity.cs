d3f1dea4691e581f7ee8246d686cbc082cd37035
Oshadi Thilakarathne <it24102608@my.sliit.lk>
2026-09-12 20:21:09 +0530

    feat(event-service): add AddTicketTypeBookedQuantity migration
    US-21

---
+++ backend/services/event-service/src/EventPulse.EventService/Migrations/20260912144751_AddTicketTypeBookedQuantity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventPulse.EventService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTicketTypeBookedQuantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookedQuantity",
+                table: "TicketTypes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookedQuantity",
+                table: "TicketTypes");
+        }
+    }
+}
